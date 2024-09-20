0ac31022eccb06b04538f0091aa68fb901480119
david antonio vigoya lópez <david.vigoya.29@gmail.com>
2024-09-19 23:57:37 -0500

    si

--- Inicio/Program.cs
+++ Inicio/Program.cs
@@ -1,39 +1,20 @@
-﻿using System;
-using System.Threading;
-/*using System.Windows.Forms;
-using VistaEscritorio;*/
-using VistaConsola;
+﻿using VistaConsola;
 
 namespace ComponenteInicio
 {
-
-
-    namespace ComponenteInicio
+    static class Program
     {
-        static class Program
+        static void Main()
         {
-            [STAThread]
-            /*static void Main()
-            {
-                // Crear e iniciar el hilo para la interfaz de consola
-                Thread consolaThread = new Thread(IniciarConsola);
-                consolaThread.Start();
+             // Ejecutar la vista de consola
+            IniciarConsola();
+        }
 
-                // Ejecutar la interfaz de escritorio en el hilo principal
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new FormMain()); // Cambia FormMain por el formulario principal de la VistaEscritorio
-
-                // Esperar a que el hilo de consola termine (opcional)
-                consolaThread.Join();
-            }*/
-
-            static void IniciarConsola()
-            {
-                VistaConsole consola = new VistaConsole();
-                consola.IniciarMenu();
-            }
+        static void IniciarConsola()
+        {
+            VistaConsole consola = new VistaConsole();
+            consola.IniciarMenu();
         }
     }
-
 }
+
